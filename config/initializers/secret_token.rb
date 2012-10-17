9895df684c53ad1691723523e49410f79a0eb4d6
unknown <mario.minutolo@gmail.com>
2012-10-17 21:23:08 +0200

    Created si2 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Si2::Application.config.secret_token = '10d7e049d9874e9e3a812e7273931398f7917fe370f70735de745568686c23cf336147e03ebf6ec4167f595937554a53bed69591b052f3736f5e4f525e88056f'
